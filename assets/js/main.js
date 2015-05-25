c4d36b291111bfe9dc300572e831eb7f480f147b
Raymond Benc <r@phpfox.com>
2015-05-25 13:34:08 +0200

    save

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,35 @@
 
 var search = false;
 $(document).ready(function() {
+
+	hljs.initHighlightingOnLoad();
+
+	var p = $('.recent-posts');
+	if (p.length && !p.hasClass('built')) {
+//   	 console.log('ready to build it...');
+		p.addClass('built');
+		$.getScript('http://community.phpfox.com/forum/recent/', function() {
+			__Threads(function(threads) {
+				var html = '', iteration = 0;
+				for (var i in threads) {
+					var t = threads[i];
+					iteration++;
+					if (iteration >= 6) {
+						break;
+					}
+					html += '<div class="list-group">';
+					html	+= '<a href="' + t.permalink + '" class="list-group-item"><h4>' + t.title + '</h4><p class="text-muted">' + t.user + '</p></a>';
+					html += '</div>';
+				}
+
+				p.html('<p class="text-muted" style="font-size:12px;">Recent Discussions</p>');
+				// p.html('');
+				p.after(html);
+				p.parents('.panel:first').after('<div><a class="btn btn-default" href="http://community.phpfox.com/" role="button">View The Community</a></div>');
+			});
+		});
+	}
+
 	$('img').magnificPopup({
 		type:'image',
 		zoom: {
@@ -38,7 +67,7 @@ $(document).ready(function() {
 	}
 
 	$('.search input').keyup(function() {
-		var t = $(this), html = '', el = $('#results'), cnt = 0;
+		var t = $(this), html = '', el = $('#results'), cnt = 0, nav = $('nav');
 
 		for (var i in search) {
 			var term = search[i];
@@ -56,9 +85,11 @@ $(document).ready(function() {
 
 		if (cnt >= 1 && t.val()) {
 			el.html(html).show();
+			nav.hide();
 		}
 		else {
 			el.html('').hide();
+			nav.show();
 		}
 	});
 });
