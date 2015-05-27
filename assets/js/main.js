45840b4810c8294536aedd893b43398c70aeae32
Raymond Benc <r@phpfox.com>
2015-05-27 17:08:41 +0200

    save

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
 
 	hljs.initHighlightingOnLoad();
 
+	$('.block-header').click(function() {
+		var t = $(this);
+
+		window.location.href = t.find('h2 a').attr('href');
+	});
+
 	var p = $('.recent-posts');
 	if (p.length && !p.hasClass('built')) {
 //   	 console.log('ready to build it...');
@@ -11,21 +17,22 @@ $(document).ready(function() {
 		$.getScript('http://community.phpfox.com/forum/recent/', function() {
 			__Threads(function(threads) {
 				var html = '', iteration = 0;
+
+				html += '<div class="list-group">';
 				for (var i in threads) {
 					var t = threads[i];
 					iteration++;
 					if (iteration >= 6) {
 						break;
 					}
-					html += '<div class="list-group">';
-					html	+= '<a href="' + t.permalink + '" class="list-group-item"><h4>' + t.title + '</h4><p class="text-muted">' + t.user + '</p></a>';
-					html += '</div>';
+					html += '<a href="' + t.permalink + '" class="list-group-item"><h4>' + t.title + '</h4><p class="text-muted">' + t.user + '</p></a>';
 				}
+				html += '</div>';
 
-				p.html('<p class="text-muted" style="font-size:12px;">Recent Discussions</p>');
+				p.html(html);
 				// p.html('');
-				p.after(html);
-				p.parents('.panel:first').after('<div><a class="btn btn-default" href="http://community.phpfox.com/" role="button">View The Community</a></div>');
+				// p.after(html);
+				// p.parents('.panel:first').after('<div><a class="btn btn-default" href="http://community.phpfox.com/" role="button">View The Community</a></div>');
 			});
 		});
 	}
@@ -90,6 +97,17 @@ $(document).ready(function() {
 		else {
 			el.html('').hide();
 			nav.show();
+		}
+	});
+
+	$('.pf-search').focus(function() {
+		var t = $(this), position = $('.form').offset();
+
+		if (!t.hasClass('focus')) {
+			t.addClass('focus');
+			$('html, body').animate({
+				scrollTop: (position.top - 65)
+			});
 		}
 	});
 });
